b9b6ea8253c327e37dd0a35adace2fb42d716505
EnigmaByte7 <saxenay117@gmail.com>
2024-07-30 01:16:31 +0530

    sv

--- extract.js
+++ extract.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer')
 
 async function Extract() {
-      const browser = await puppeteer.launch();
+      const browser = await puppeteer.launch({headless:true});
       const page = await browser.newPage();
 
       await page.goto('https://www.jecjabalpur.ac.in');
