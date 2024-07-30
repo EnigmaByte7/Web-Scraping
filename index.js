10a01c14c6381491afc4f9343086fb1ad79fc5f1
EnigmaByte7 <saxenay117@gmail.com>
2024-07-30 19:03:34 +0530

    sv

--- index.js
+++ index.js
@@ -2,18 +2,30 @@ const nodeCron = require('node-cron')
 const {Connect, Save} = require('./db')
 const {Extract} =  require('./extract')
 const {Notifier} = require('./notifier')
+const express = require('express');
+const app = express();
+
+app.get('/', (req,res)=>{
+    res.send('Automation service is running!');
+})
 
 async function connect() { await Connect() }
 
 connect();
-async function main(){
-    const notices = await Extract();
-    const res = await Save(notices);
-    console.log(res);
-    if(res === 1) console.log('Happy !');
-    if(res != 1){
-        Notifier(res);
+nodeCron.schedule("*/5 * * * *", ()=> {
+    async function main(){
+        const notices = await Extract();
+        const res = await Save(notices);
+        console.log(res);
+        if(res === 1) console.log('Happy !');
+        if(res != 1){
+            Notifier(res);
+        }
     }
-}
-main();
+})
+
+
+app.listen(1000, () => {
+    console.log(`Example app listening on port 1000`)
+  })
 
